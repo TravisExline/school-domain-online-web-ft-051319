9800e248728edf3be900c2cb8643767eb5f71a5a
Travis Exline <t.exline22@gmail.com>
2019-05-17 16:49:51 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -18,8 +18,8 @@ class School
   def sort
     sorted = {}
     @roster.each do |grade, name|
-      sorted[grade] = name
+      sorted[grade] = name.sort
     end
-    sorted.sort
+    sorted
   end
 end
